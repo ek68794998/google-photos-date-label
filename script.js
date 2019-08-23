4004f89ee81487b9e973ab5db99e504f7ebd367e
Eric <eric.kumlin@gmail.com>
2019-08-23 16:11:49 -0700

    Add gate to prevent thumbnails from having two date labels

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 function appendDateLabelToPhoto(target) {
+  var photoContainer = target.parentElement && target.parentElement.parentElement;
+  if (!photoContainer || !photoContainer.classList.contains("nV0gYe")) {
+    // Prevent thumbnails from having two date labels added.
+    return;
+  }
+
   var className = "dateLabel";
 
   var existingDateLabels = target.getElementsByClassName(className);
   if (existingDateLabels && existingDateLabels.length > 0) {
+    // Prevent a second date label from being added.
     return;
   }
 
   var text =
-    target.parentElement
-    && target.parentElement.attributes
+    target.parentElement.attributes
     && target.parentElement.attributes["aria-label"];
 
   if (text && text.value) {
+    // The field comes as something like "Photo - Landscape - [datestamp]", so split it.
     var labelFields = text.value.split(" - ");
+
     target.insertAdjacentHTML(
       "afterBegin",
       "<div class='" + className + "' style='opacity: 0'>" + labelFields[labelFields.length - 1] + "</div>");
